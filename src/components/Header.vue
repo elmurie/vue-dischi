9c60a33ac81f341505957d8bccfc3817eb89b8f3
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-02 12:40:22 +0200

    added header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,24 @@
 <template>
-    <header></header>
+    <header class="container">
+        <img :src="logoImg" alt="Logo">
+    </header>
 </template>
 
 <script>
 export default {
     name : "Header",
+    props : ['logoImg']
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/variables';
+
+header {
+    background-color: $lightColor;
+    padding: 15px;
+    height: 80px;
+    display: flex;
+}
 
 </style>
